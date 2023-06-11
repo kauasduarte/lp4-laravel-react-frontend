e8c571b5ace9ad1ced6fc3af836ebb5e500490f0
Gabriel Gomes <gabrielgomes1651@outlook.com>
2023-06-11 20:47:07 -0300

    add front

--- src/components/menu/Header.jsx
+++ src/components/menu/Header.jsx
@@ -1,12 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Post } from '../crud/Post';
-import { PostList } from '../crud/PostList';
 import { PostEdit } from '../crud/PostEdit';
 import { PostDelet } from '../crud/PostDelet';
 import { Menu } from './Menu';
 import { Produtos } from '../crud/GetProdutos';
-
-import { Sidebar } from '../sidebar/Sidebar.jsx';
 import styles from './Menu.module.css'
 
 export function Header() {
@@ -16,15 +13,13 @@ export function Header() {
                 <Menu />
                 <div>
                     <div className={styles.wrapper}>
-                        <Sidebar />
-
                         <main>
                             <Routes>
-                                <Route path="/" element={<PostList />} />
+                                <Route path="/" element={<Produtos />} />
                                 <Route path="/produtos/create" element={<Post />} />
                                 <Route path="/produtos/edit/:id" element={<PostEdit />} />
                                 <Route path="/produtos/delete/:id" element={<PostDelet />} />
-                                <Route path="/produtos/" element={<Produtos />} />
+                                <Route path="/produtos" element={<Produtos />} />
                             </Routes>
                         </main>
                     </div>
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,8 +4,9 @@ import styles from './Menu.module.css'
 export function Menu() {
     return (
         <header className={styles.header}>
+            <h1>Mira Market</h1>
             <div>
-                <Link className={styles.menuText} to="/"> Home </Link>
+                
                 <Link className={styles.menuText} to="/produtos"> Lista de Produtos </Link>
                 <Link className={styles.menuText} to="/produtos/create"> Cadastrar Produto </Link>                
             </div>
