d0c74299befa831985a1b4415a8d9789c5d7ef2c
Gabriel Gomes <gabrielgomes1651@outlook.com>
2023-06-11 21:16:19 -0300

    clean css

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -6,7 +6,6 @@ export function Menu() {
         <header className={styles.header}>
             <h1>Mira Market</h1>
             <div>
-                
                 <Link className={styles.menuText} to="/produtos"> Lista de Produtos </Link>
                 <Link className={styles.menuText} to="/produtos/create"> Cadastrar Produto </Link>                
             </div>
